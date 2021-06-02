1b89dbcd4850a4cd74dfd389853f90a41bbe2952
Gustavo Leite <gustavo.leite@zup.com.br>
2021-06-02 16:58:22 -0300

    Add MySQL database

--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -1,10 +1,10 @@
 package academy.devdojo.springboot2.repository;
 
 import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 // Diz quais são as queries que podem ser feitas com esse modelo no banco de dados
-public interface AnimeRepository {
-    List<Anime> listAll();
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
 }
