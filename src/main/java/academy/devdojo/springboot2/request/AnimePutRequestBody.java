8ce478c8c44c5615fe7f8c7160b99985d4c41f2f
Gustavo Leite <gustavo.leite@zup.com.br>
2021-06-04 11:34:07 -0300

    Add validation to replace method

--- src/main/java/academy/devdojo/springboot2/request/AnimePutRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/request/AnimePutRequestBody.java
@@ -2,8 +2,11 @@ package academy.devdojo.springboot2.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePutRequestBody {
     private Long id;
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 }
