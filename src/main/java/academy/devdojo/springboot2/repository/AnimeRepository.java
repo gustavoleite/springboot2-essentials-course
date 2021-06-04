509f6b9a04b46d8c5012719721098a9e2c2c1eb6
Gustavo Leite <gustavo.leite@zup.com.br>
2021-06-04 10:54:54 -0300

    Request params class

--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-// Diz quais são as queries que podem ser feitas com esse modelo no banco de dados
+// Diz quais são as queries além das padrões podem ser feitas com esse modelo no banco de dados
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
+
+    List<Anime> findByName(String name);
 }
